304b0c705d8a3e766bfa4889cfa624b2cabc5918
Orest Vaheman <ovaheman@gmail.com>
2025-05-27 22:20:05 +0300

    sort list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const item = Array.from(list.querySelectorAll('li'));
+
+function sortList(data) {
+  const sortedItems = data.sort((a, b) => {
+    const salaryA = getNumberOfString(a.getAttribute('data-salary'));
+    const salaryB = getNumberOfString(b.getAttribute('data-salary'));
+
+    return salaryB - salaryA;
+  });
+
+  list.innerHTML = '';
+  sortedItems.forEach((element) => list.appendChild(element));
+
+  return sortedItems;
+}
+
+sortList(item);
+
+function getNumberOfString(string) {
+  let number = '';
+
+  for (const el of string) {
+    if (!isNaN(el)) {
+      number += el;
+    }
+  }
+
+  return +number;
+}
